6027260bc7cf037b88cb3e4e9cb3f3816bdb2a26
Dani <s.leslie94@gmail.com>
2022-08-30 12:16:09 -0400

    add win conditions and stop game after they are met

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const gameBoard = (() => {
+let gameActive = true;
 const playerCreation = (marker, turn) => {
     return {marker, turn};
 }
@@ -8,6 +9,10 @@ const playerCreation = (marker, turn) => {
 const playerX = playerCreation('X', true);
 const playerO = playerCreation('O', false);
 let currentPlayer = playerX;
+const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+                       [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+                       [0, 4, 8], [2, 4, 6]]
+
 
 
 const generateBoard = (() => {
@@ -42,12 +47,36 @@ const boardUpdate = (index) =>{
     console.log(board);
 }
 
+const gameResult = () => {
+    // search for a win condition in each round
+    let winningRound = false;
+    for (let i = 0; i <=7; i++){
+        let winCondition1 = board[winConditions[i][0]];
+        let winCondition2 = board[winConditions[i][1]];
+        let winCondition3 = board[winConditions[i][2]];
+        
+        if(!winCondition1 || !winCondition2 || !winCondition3 ){
+            continue;
+        }
+        if (winCondition1 === winCondition2 && winCondition2 === winCondition3) {
+            winningRound = true;
+            break;
+        }
+    }
+    if (winningRound) {
+        console.log(`Round won by ${currentPlayer.marker}`);
+        gameActive = false;
+        return;
+    }
+}
+
 const playerTest = (square, index) => {
-    if (validMove(square)){
+    if (validMove(square) && gameActive){
     square.innerText = currentPlayer.marker;
     boardUpdate(index);
+    gameResult();
     changePlayer();
-}
+    }
 }
 
 const squares = document.querySelectorAll('.square');
